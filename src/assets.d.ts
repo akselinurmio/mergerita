df8282cd9f7dd44524ae66a85ed07ede01661cff
Akseli Nurmio <akseli.nurmio@yle.fi>
2026-03-27 22:35:11 +0200

    Add favicon

---
+++ src/assets.d.ts
@@ -0,0 +1,9 @@
+declare module "*.html" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: ArrayBuffer;
+  export default content;
+}
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import { Hono } from "hono";
 import { handleWebhook } from "./webhook";
 import type { Env } from "./types";
 import indexHtml from "./index.html";
+import favicon from "../static/cocktail.png";
 
 const app = new Hono<{ Bindings: Env }>();
 
 app.get("/", (c) => c.html(indexHtml));
+app.get("/favicon.ico", (c) => {
+  return c.body(favicon, { headers: { "Content-Type": "image/png" } });
+});
 app.post("/webhook", handleWebhook);
 
 export default app;
